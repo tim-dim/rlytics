30c6841c761ee8b3e3d1edef1c608e108710cc96
OCCGER\Tim Eck <tim.eck@occstrategy.de>
2017-09-07 10:57:44 +0200

    first commit

---
+++ R/ggthemes_data.R
@@ -0,0 +1,29 @@
+#' Palette data for the rlytics package
+#'
+#' Data used by the palettes in the rlytics package.
+#'
+#' @format A \code{list}.
+#' @export
+ggthemes_data <- {
+  ## x to hold value of list as I create it
+  x <- list()
+
+  x$occ <-    c(akzent2 = "#99BAD9",
+                    akzent3 = "#669DC5",
+                    akzent4 = "#3375B2",
+                    akzent5 = "#FF9933",
+                    akzent6 = "#C0C0C0",
+                    hell2 = "#00539F",
+                    dunktel2 = "#002C54",
+                    akzent1 = "#CCDDEC")
+
+  x$colorblind <- c(black = "#000000",
+                    orange = "#E69F00",
+                    sky_blue = "#56B4E9",
+                    bluish_green = "#009E73",
+                    yellow = "#F0E442",
+                    blue = "#0072B2",
+                    vermillion = "#D55E00",
+                    reddish_purple = "#CC79A7")
+  x
+}
---
+++ R/scale_colour_occ.R
@@ -0,0 +1,32 @@
+#' occ Color Palette (Discrete) and Scales
+#'
+#' An 8-color occ safe qualitative discrete palette.
+#'
+#' @rdname occ
+#' @references
+#' Umrechnung OCC-Farbschema in Hex-Werte.pptx
+#'
+#'
+#' @export
+#' @inheritParams ggplot2::scale_colour_hue
+#' @family colour
+#' @example inst/examples/ex-occ.R
+occ_pal <- function() {
+  scales::manual_pal(unname(ggthemes_data$occ))
+}
+
+#' @rdname occ
+#' @export
+scale_colour_occ <- function(...) {
+  discrete_scale("colour", "occ", occ_pal(), ...)
+}
+
+#' @rdname occ
+#' @export
+scale_color_occ <- scale_colour_occ
+
+#' @rdname occ
+#' @export
+scale_fill_occ <- function(...) {
+  discrete_scale("fill", "occ", occ_pal(), ...)
+}
