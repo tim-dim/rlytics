b6c7b2e09a4933a6ed82766624281cc247094d83
Tim Eck <DETECK01@de.eurw.ey.net>
2018-02-05 14:12:54 +0100

    added functions

--- R/rlytics-package.R
+++ R/rlytics-package.R
@@ -26,3 +26,22 @@ ggname <- function (prefix, grob) {
 }
 
 globalVariables("ggthemes_data")
+
+# assembles a ey-p style filename
+propperFilename <- function(name = "test", project="51103", path="", kuerzel="JSA", fileType = "xlsx") {
+  date = format(Sys.Date(),"%y%m%d")
+  paste(paste(project, date, kuerzel, sep="-"), " ", name, ".", fileType, sep="")
+}
+
+# cleans out umlaute
+clean_umlaute <- function (x) {
+  x = gsub("â€“", "–", x)
+  x = gsub("Ãœ", "Ü", x)
+  x = gsub("Ã¤", "ä", x)
+  x = gsub("Â»", "»", x)
+  x = gsub("Ã¼", "ü", x)
+  x = gsub("â€˜", "‘", x)
+  x = gsub("Ã¶", "ö", x)
+  x = gsub("âˆ’", "?", x)
+  x
+}
